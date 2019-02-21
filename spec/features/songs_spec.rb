43c99424cde26d804268fb2e608c0b6d03151638
o-vuong <oscarvuong@gmail.com>
2019-02-21 05:41:12 -0800

    Done.

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -8,10 +8,7 @@ describe "songs", type:  :feature do
     @song = @artist.songs.create!(title: "The Grid")
   end
 
-  it "lists songs in 'Artist Name - Song Title' format" do
-    visit songs_path
-    expect(page).to have_link("Daft Punk - The Grid", href: song_path(@song))
-  end
+
 
   it "links to the artist" do
     visit song_path(@song)
